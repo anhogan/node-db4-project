de6b6eac5020f13297ee4345fb1a7eeb3c80b111
Amanda Hogan <ahogan@teamworks.com>
2020-03-19 10:51:38 -0400

    Setup seed files

--- data/seeds/02-ingredients.js
+++ data/seeds/02-ingredients.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('ingredients').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('ingredients').insert([
+        {id: 1, name: 'Cup of Quick Cooking Oats', quantity: 4},
+        {id: 2, name: 'Cup of Milk', quantity: 4},
+        {id: 3, name: 'Package of Chocolate Chip Cookie Dough', quantity: 2},
+        {id: 4, name: 'Quarter Cup of Dried Cranberries', quantity: 2.5},
+        {id: 5, name: 'Quarter Cup of Walnuts', quantity: 2.5},
+        {id: 6, name: 'Teaspoon of Cinnamon', quantity: 8}
       ]);
     });
 };
--- data/seeds/03-instructions.js
+++ data/seeds/03-instructions.js
@@ -1,13 +1,17 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('instructions').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('instructions').insert([
+        {id: 1, step_number: 1, description: 'Pour oatmeal into a slow cooker', recipe_id: 1},
+        {id: 2, step_number: 2, description: 'Add in milk', recipe_id: 1},
+        {id: 3, step_number: 3, description: 'Add in your favorite oatmeal toppings', recipe_id: 1},
+        {id: 4, step_number: 4, description: 'Turn the slow cooker on low heat overnight', recipe_id: 1},
+        {id: 5, step_number: 1, description: 'Open the package of store-bought cookie dough', recipe_id: 2},
+        {id: 6, step_number: 2, description: 'Place cookies on a baking sheet', recipe_id: 2},
+        {id: 7, step_number: 3, description: 'Bake for amount of time on package', recipe_id: 2}
       ]);
     });
 };
